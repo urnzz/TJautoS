14b40d2d11a3d16de05dd60650142cff50328388
urnz <rafaelsansana25@hotmail.com>
2022-08-30 16:28:51 -0300

    wip

--- main.py
+++ main.py
@@ -16,35 +16,36 @@ def main():
             url='https://esaj.tjsp.jus.br/cpopg/search.do?conversationId=&cbPesquisa=DOCPARTE&dadosConsulta.valorConsulta={}&cdForo=-1'
             driver.get(url.format(cpf))
             try:
-                WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="listagemDeProcessos"]')))
+                WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="listagemDeProcessos"]')))
                 t=driver.find_element(By.XPATH, '//*[@id="listagemDeProcessos"]')
                 lists = t.find_elements(By.TAG_NAME, "ul")
                 for list in lists:
                     rows = list.find_elements(By.TAG_NAME, 'li')
                     n=-1
                     enc=False
+                    numQ=''
+                    numS=''
                     for row in rows:
                         n+=1
                         if "Cumprimento de sentença" in str(row.find_element(By.CLASS_NAME, "classeProcesso").text):
                             if '2019' in str(row.find_element(By.CLASS_NAME, "linkProcesso").text) or '2020' in str(row.find_element(By.CLASS_NAME, "linkProcesso").text) or '2021' in str(row.find_element(By.CLASS_NAME, "linkProcesso").text) or '2022' in str(row.find_element(By.CLASS_NAME, "linkProcesso").text):
                                 print('encontrado quinquenio')
-                                o.write(cpf.strip()+", QUINQUENIO:"+row.find_element(By.CLASS_NAME, "linkProcesso").text+"\n")
                                 enc=True
+                                numQ=row.find_element(By.CLASS_NAME, "linkProcesso").text
                             elif '2011' in str(row.find_element(By.CLASS_NAME, "linkProcesso").text):
                                 print('encontrado sexta-parte')
-                                o.write(cpf.strip()+", SEXTA-PARTE:"+row.find_element(By.CLASS_NAME, "linkProcesso").text+"\n")
                                 enc=True
+                                numS=row.find_element(By.CLASS_NAME, "linkProcesso").text
                             elif '2005' in str(row.find_element(By.CLASS_NAME, "linkProcesso").text):
                                 print('encontrado quinquenio')
-                                o.write(cpf.strip()+", QUINQUENIO:"+row.find_element(By.CLASS_NAME, "linkProcesso").text+"\n")
                                 enc=True
+                                numQ=row.find_element(By.CLASS_NAME, "linkProcesso").text
                             elif n == len(rows) and enc==False:
-                                o.write(cpf.strip()+",sem processo\n")
                                 print("sem processo") 
                         else:
                             if n == len(rows) and enc==False:
-                                o.write(cpf.strip()+",sem processo\n")
                                 print("sem processo") 
+                        o.write(cpf.strip()+',q='+numQ+',s='+numS)
             except:
                 o.write(cpf.strip()+",so 1 proc\n")
                 print("so 1 proc") 
