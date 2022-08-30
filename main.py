bcee4ef2976413ac4de6339f230c9ab0e798dc98
urnz <rafaelsansana25@hotmail.com>
2022-08-30 16:13:18 -0300

    wip

--- main.py
+++ main.py
@@ -12,36 +12,42 @@ def main():
     driver = uc.Chrome(options=options)
     for cpf in cpfs:
         try:
-            print("pesquisando no PJE "+cpf.strip())
-            driver.get("https://www.google.com/search?client=firefox-b-d&q=consulta+pje")
-            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[7]/div/div[10]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/a/h3")))
-            gs = driver.find_element(By.XPATH, "/html/body/div[7]/div/div[10]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/a/h3")
-            gs.click()
-            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="fPP:dpDec:documentoParte"]')))
-            cpfC = driver.find_element(By.XPATH, '//*[@id="fPP:dpDec:documentoParte"]')
-            cpfC.click()
-            cpfC.clear()
-            cpfC.send_keys(cpf.strip())
-            driver.find_element(By.XPATH, '//*[@id="fPP:searchProcessos"]').click()
+            print("pesquisando no TJ "+cpf.strip())
+            url='https://esaj.tjsp.jus.br/cpopg/search.do?conversationId=&cbPesquisa=DOCPARTE&dadosConsulta.valorConsulta={}&cdForo=-1'
+            driver.get(url.format(cpf))
             try:
-                WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[5]/div/div/div/div[2]/form/div[2]/div/table/tbody/tr')))
-                t=driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div/div[2]/form/div[2]/div/table/tbody')
-                rows = t.find_elements(By.TAG_NAME, "tr")
-                for row in rows:
-                    if "teto" in str(row.find_element(By.CLASS_NAME, "btn-block").text):
-                        print('encontrado')
-                        o.write(cpf.strip()+","+row.find_element(By.CLASS_NAME, "btn-block").text+"\n")
-                        break
-                    else:
-                        if "Teto" in str(row.find_element(By.CLASS_NAME, "btn-block").text):
-                            print('encontrado')
-                            o.write(cpf.strip()+","+row.find_element(By.CLASS_NAME, "btn-block").text+"\n")
-                            break
+                WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="listagemDeProcessos"]')))
+                t=driver.find_element(By.XPATH, '//*[@id="listagemDeProcessos"]')
+                lists = t.find_elements(By.TAG_NAME, "ul")
+                for list in lists:
+                    rows = list.find_elements(By.TAG_NAME, 'li')
+                    n=-1
+                    enc=False
+                    for row in rows:
+                        n+=1
+                        if "Cumprimento de sentença" in str(row.find_element(By.CLASS_NAME, "classeProcesso").text):
+                            if '2019' in str(row.find_element(By.CLASS_NAME, "linkProcesso").text) or '2020' in str(row.find_element(By.CLASS_NAME, "linkProcesso").text) or '2021' in str(row.find_element(By.CLASS_NAME, "linkProcesso").text) or '2022' in str(row.find_element(By.CLASS_NAME, "linkProcesso").text):
+                                print('encontrado quinquenio')
+                                o.write(cpf.strip()+", QUINQUENIO:"+row.find_element(By.CLASS_NAME, "linkProcesso").text+"\n")
+                                enc=True
+                            elif '2011' in str(row.find_element(By.CLASS_NAME, "linkProcesso").text):
+                                print('encontrado sexta-parte')
+                                o.write(cpf.strip()+", SEXTA-PARTE:"+row.find_element(By.CLASS_NAME, "linkProcesso").text+"\n")
+                                enc=True
+                            elif '2005' in str(row.find_element(By.CLASS_NAME, "linkProcesso").text):
+                                print('encontrado quinquenio')
+                                o.write(cpf.strip()+", QUINQUENIO:"+row.find_element(By.CLASS_NAME, "linkProcesso").text+"\n")
+                                enc=True
+                            elif n == len(rows) and enc==False:
+                                o.write(cpf.strip()+",sem processo\n")
+                                print("sem processo") 
                         else:
-                            raise Exception('') 
+                            if n == len(rows) and enc==False:
+                                o.write(cpf.strip()+",sem processo\n")
+                                print("sem processo") 
             except:
-                o.write(cpf.strip()+",\n")
-                print("sem processo de teto") 
+                o.write(cpf.strip()+",so 1 proc\n")
+                print("so 1 proc") 
         except:
             o.write(cpf.strip()+",erro\n")
             print('erro encontrado, pulando cpf: '+cpf)
